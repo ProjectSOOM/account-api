23d6f47928cc1ea564e1b57c9f86b7f51b1acd9e
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-03-08 20:35:11 +0900

    [ADD] JWT 유틸 작성

---
+++ src/main/java/com/soom/account_api/global/util/JwtUtil.java
@@ -0,0 +1,28 @@
+package com.soom.account_api.global.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public class JwtUtil {
+    private static final SignatureAlgorithm ALGORITHM = SignatureAlgorithm.HS256;
+
+    public static String encode(String secret, Long expiredSeconds, Map<String, Object> claims) {
+        final LocalDateTime now = LocalDateTime.now();
+        final LocalDateTime expiredDate = now.plusSeconds(expiredSeconds);
+        return Jwts.builder()
+                .setIssuedAt(Timestamp.valueOf(now))
+                .setExpiration(Timestamp.valueOf(expiredDate))
+                .setClaims(claims)
+                .signWith(ALGORITHM, secret)
+                .compact();
+    }
+
+    public static Claims decode(String secret, String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+}
