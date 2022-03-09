6dd73c33abfc7b8d3b6b20361812480a22edc918
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-03-09 17:31:13 +0900

    [ADD] 학교이메일관련 검증로직 작성

---
+++ src/main/java/com/soom/account_api/domain/sign/validation/UniqueSchoolEmailValidator.java
@@ -0,0 +1,22 @@
+package com.soom.account_api.domain.sign.validation;
+
+import com.soom.account_api.domain.sign.service.SchoolEmailValidService;
+import com.soom.account_api.global.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+@Component
+@RequiredArgsConstructor
+public class UniqueSchoolEmailValidator implements ConstraintValidator<UniqueSchoolEmail, String> {
+    private final AccountRepository accountRepository;
+    private final SchoolEmailValidService schoolEmailVaildService;
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+        return schoolEmailVaildService.isSchoolEmail(email)
+                && !accountRepository.existsByEmail(email);
+    }
+}
