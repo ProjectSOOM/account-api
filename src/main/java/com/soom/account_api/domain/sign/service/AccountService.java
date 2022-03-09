434c44b6bb80fb63c976650e09c43c7bbfd14ad1
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-03-09 11:38:09 +0900

    [ADD] AccountService 명세작성

---
+++ src/main/java/com/soom/account_api/domain/sign/service/AccountService.java
@@ -0,0 +1,12 @@
+package com.soom.account_api.domain.sign.service;
+
+import com.soom.account_api.domain.sign.data.dto.StudentSignupInfoDto;
+import com.soom.account_api.domain.sign.data.dto.TeacherSignupInfoDto;
+import com.soom.account_api.domain.sign.data.dto.WithdrawalInfoDto;
+
+public interface AccountService {
+    void signUp(StudentSignupInfoDto dto);
+    void signUp(TeacherSignupInfoDto dto);
+
+    void withdrawal(WithdrawalInfoDto dto);
+}
