d432beae333e73b646601ee44c26281aa282b3d5
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-03-09 11:32:42 +0900

    [ADD] SignupController 에 필요한 Request 객체 작성

---
+++ src/main/java/com/soom/account_api/domain/sign/data/request/TeacherSignupRequest.java
@@ -0,0 +1,17 @@
+package com.soom.account_api.domain.sign.data.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.soom.account_api.domain.sign.data.type.TeacherType;
+
+import java.time.LocalDate;
+
+public record TeacherSignupRequest(
+        String emailToken,
+        String password,
+        String name,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+        LocalDate birth,
+        String code,
+        TeacherType teacher
+) {
+}
---
+++ src/main/java/com/soom/account_api/domain/sign/data/request/WithdrawalRequest.java
@@ -0,0 +1,4 @@
+package com.soom.account_api.domain.sign.data.request;
+
+public record WithdrawalRequest(String emailToken, String password) {
+}
