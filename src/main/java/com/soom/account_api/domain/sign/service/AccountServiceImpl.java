3bd95b687215c6dada0542f2cb0ac58a68d92e97
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-03-10 20:27:17 +0900

    [ADD] 회원가입시 정책 검사 로직 추가

--- src/main/java/com/soom/account_api/domain/sign/service/AccountServiceImpl.java
+++ src/main/java/com/soom/account_api/domain/sign/service/AccountServiceImpl.java
@@ -3,6 +3,9 @@ package com.soom.account_api.domain.sign.service;
 import com.soom.account_api.domain.sign.data.dto.StudentSignupInfoDto;
 import com.soom.account_api.domain.sign.data.dto.TeacherSignupInfoDto;
 import com.soom.account_api.domain.sign.data.dto.WithdrawalInfoDto;
+import com.soom.account_api.domain.sign.data.type.SignupPolicyType;
+import com.soom.account_api.domain.sign.exception.PolicyViolationException;
+import com.soom.account_api.domain.sign.policy.SignupPolicy;
 import com.soom.account_api.global.entity.StudentEntity;
 import com.soom.account_api.global.entity.TeacherEntity;
 import com.soom.account_api.global.repository.AccountRepository;
@@ -12,6 +15,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
 @RequiredArgsConstructor
 public class AccountServiceImpl implements AccountService{
@@ -19,11 +24,13 @@ public class AccountServiceImpl implements AccountService{
     private final AccountRepository accountRepository;
     private final StudentRepository studentRepository;
     private final TeacherRepository teacherRepository;
+    private final SignupPolicy signupPolicy;
 
     @Override
-    public void signUp(StudentSignupInfoDto dto) {
-        String encodedPassword = passwordEncoder.encode(dto.authInfo().password());
-        StudentEntity entity = new StudentEntity(
+    public void signUp(final StudentSignupInfoDto dto) {
+        checkPolicy(dto);
+        final String encodedPassword = passwordEncoder.encode(dto.authInfo().password());
+        final StudentEntity entity = new StudentEntity(
                 dto.authInfo().email(), encodedPassword, //AuthInfo
                 dto.profileInfo().name(), dto.profileInfo().birth(), //ProfileInfo
                 dto.studentInfo().admissionYear(), dto.studentInfo().schoolNumber(), dto.studentInfo().department() //StudentInfo
@@ -32,14 +39,34 @@ public class AccountServiceImpl implements AccountService{
     }
 
     @Override
-    public void signUp(TeacherSignupInfoDto dto) {
-        String encodedPassword = passwordEncoder.encode(dto.authInfo().password());
-        TeacherEntity entity = new TeacherEntity(
+    public void signUp(final TeacherSignupInfoDto dto) {
+        checkPolicy(dto);
+        final String encodedPassword = passwordEncoder.encode(dto.authInfo().password());
+
+        final TeacherEntity entity = new TeacherEntity(
                 dto.authInfo().email(), encodedPassword, //AuthInfo
                 dto.profileInfo().name(), dto.profileInfo().birth(), //ProfileInfo
                 dto.teacherInfo().code(), dto.teacherInfo().teacher() // TeacherInfo
         );
         teacherRepository.save(entity);
+    }
+
+    private void checkPolicy(final TeacherSignupInfoDto dto) {
+        checkPolicy(dto.authInfo().email(), dto.authInfo().password(), dto.profileInfo().name(), dto.profileInfo().birth());
+        if(!signupPolicy.checkTeacherCode(dto.teacherInfo().code())) throw new PolicyViolationException(SignupPolicyType.TEACHER_CODE_POLICY);
+    }
+
+    private void checkPolicy(final StudentSignupInfoDto dto) {
+        checkPolicy(dto.authInfo().email(), dto.authInfo().password(), dto.profileInfo().name(), dto.profileInfo().birth());
+        if(!signupPolicy.checkStudentAdmissionYear(dto.studentInfo().admissionYear())) throw new PolicyViolationException(SignupPolicyType.TEACHER_CODE_POLICY);
+        if(!signupPolicy.checkStudentSchoolNumber(dto.studentInfo().schoolNumber())) throw new PolicyViolationException(SignupPolicyType.TEACHER_CODE_POLICY);
+    }
+
+    private void checkPolicy(String email, String password, String name, LocalDate birth) {
+        if(!signupPolicy.checkEmailPolicy(email)) throw new PolicyViolationException(SignupPolicyType.EMAIL_POLICY);
+        if(!signupPolicy.checkPasswordPolicy(password)) throw new PolicyViolationException(SignupPolicyType.PASSWORD_POLICY);
+        if(! signupPolicy.checkNamePolicy(name)) throw new PolicyViolationException(SignupPolicyType.NAME_POLICY);
+        if(!signupPolicy.checkBirthPolicy(birth)) throw new PolicyViolationException(SignupPolicyType.BIRTH_POLICY);
     }
 
     @Override
